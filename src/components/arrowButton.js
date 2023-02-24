cb1939c5b144bc439ff8a31760cc5c284e50c2ec
raulariias <raularias24685@gmail.com>
2023-02-24 17:13:33 -0400

    first commit

---
+++ src/components/arrowButton.js
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import styles from "@styles/components/arrowButton.module.scss";
+
+const arrowRight = <FontAwesomeIcon icon={faArrowRight} />;
+
+const ArrowButton = () => {
+  return <button className={`${styles.arrow}`}>{arrowRight}</button>;
+};
+
+export default ArrowButton;
---
+++ src/hooks/useTyped.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Typed from "typed.js";
+
+const TypedReactHook = () => {
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = React.useRef(null);
+
+  React.useEffect(() => {
+    const options = {
+      strings: [
+        "Hola 👋",
+        `Bienvenid@ a mi libreria 
+de retos de FM🎨`,
+        "Espero sea de tu agrado 👨‍🚀",
+      ],
+      typeSpeed: 10,
+      backSpeed: 10,
+    };
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <span style={{ whiteSpace: "pre", textAlign: "center" }} ref={el} />
+      </div>
+    </div>
+  );
+};
+
+export default TypedReactHook;
